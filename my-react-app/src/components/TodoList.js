221667fbe923a844b93fe575dbb2e19573886cda
Matsuda1129 <mk1529ayukat315takoyaki@yahoo.co.jp>
2020-10-25 21:47:55 +0900

    develop7

--- my-react-app/src/components/TodoList.js
+++ my-react-app/src/components/TodoList.js
@@ -3,10 +3,10 @@ import TodoItem from './TodoItem';
 
 const TodoList = (props) => {
   const todos = props.todos.map((todo, index) => {
-     if(props.radio[0].checked == true){
-     } else if(props.radio[1].checked == true){
+     if(props.radio[0].checked === true){
+     } else if(props.radio[1].checked === true){
       if(todo.status === '完了'){return}
-     }else if(props.radio[2].checked == true){
+     }else if(props.radio[2].checked === true){
       if(todo.status === '作業中'){return}
      }
     return (
